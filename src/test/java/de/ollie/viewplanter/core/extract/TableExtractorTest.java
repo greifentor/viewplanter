d20df84928c6e2ba5a241b18d2f24461d3235048
Oliver Lieshoff <o.lieshoff@schuetze-consulting.ag>
2023-03-02 09:54:48 +0100

    Created test scenarios for with statements.

--- src/test/java/de/ollie/viewplanter/core/extract/TableExtractorTest.java
+++ src/test/java/de/ollie/viewplanter/core/extract/TableExtractorTest.java
@@ -12,7 +12,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import de.ollie.viewplanter.core.extract.TableExtractor;
 import de.ollie.viewplanter.core.model.TableData;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,6 +20,8 @@ public class TableExtractorTest {
 	private static final TableData TABLE_A = new TableData().setName("TableA");
 	private static final TableData TABLE_B = new TableData().setName("TableB");
 	private static final TableData TABLE_C = new TableData().setName("TableC");
+	private static final TableData WITH_C = new TableData().setName("WithC");
+	private static final TableData WITH_VIEW_B = new TableData().setName("WithViewB");
 
 	@InjectMocks
 	private TableExtractor unitUnderTest;
@@ -85,6 +86,22 @@ public class TableExtractorTest {
 							.extract(
 									"select * from (" + TABLE_A.getName() + " join " + TABLE_B.getName()
 											+ " on a.id = b.id)"));
+		}
+
+		@Test
+		void returnsACorrectListWithTableData_passingASelectStatementWithAWithStatement() {
+			assertEquals(List.of(TABLE_A, TABLE_B, TABLE_C),
+					unitUnderTest.extract(
+							"with " + WITH_C.getName() + " as (select * from " + TABLE_C.getName() + ") select * from ("
+									+ TABLE_A.getName() + " join " + TABLE_B.getName() + " on a.id = b.id)"));
+		}
+
+		@Test
+		void returnsACorrectListWithTableData_passingASelectStatementWithAWithStatementReferencingAPartOfTheWithStatement() {
+			assertEquals(List.of(TABLE_A, TABLE_B, TABLE_C),
+					unitUnderTest.extract("with " + WITH_C.getName() + " as (select * from " + TABLE_C.getName() + "), "
+							+ WITH_VIEW_B.getName() + " as (select * from " + TABLE_B.getName() + ", "
+							+ WITH_C.getName() + ") select * from " + TABLE_A.getName() + ", " + TABLE_B.getName()));
 		}
 
 	}
